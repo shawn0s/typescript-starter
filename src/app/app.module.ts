75bfb676fffae2c74f9f1142df154cdefbcb8295
shawn0s <57805297+shawn0s@users.noreply.github.com>
2020-05-04 23:09:05 +0800

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module,NestModule, MiddlewareConsumer,RequestMethod } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './app/users/users.module';
+import { UsersModule } from '../app/users/users.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { CatsModule } from './app/cats/cats.module';
+import { CatsModule } from '../app/cats/cats.module';
+import { AuthGuard } from './auth/auth.guard';
+
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest'),
@@ -13,4 +15,11 @@ import { CatsModule } from './app/cats/cats.module';
             }),
           ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthGuard)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+
+}
